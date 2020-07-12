418d4575a9b1f1398f730fc710c026af0d0dba93
pabloxcastro <pablo.castro@hotmail.com>
2020-07-12 09:15:29 -0300

    app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const { logger } = require("./config/logger.js");
-const { controller } = require("./controllers/gradeController.js");
+const { logger } = require('./config/logger.js');
+const { controller } = require('./controllers/gradeController.js');
 //const { rotas } = require("./routes/gradeRouter.js");
 
 try {
@@ -13,7 +13,7 @@ try {
     useUnifiedTopology: true,
     useFindAndModify: false,
   });
-  logger.info("Conectado ao banco de dados");
+  logger.info('Conectado ao banco de dados');
 } catch (error) {
   logger.error(`Erro ao conectar no banco de dados! ${error}`);
 }
@@ -25,21 +25,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: 'http://localhost:8080',
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("API em execucao");
+app.get('/', (req, res) => {
+  res.send('API em execucao');
 });
 
 //app.use(rotas);
-app.post("/grade", controller.create);
-app.get("/grade/:id", controller.findOne);
-app.get("/grade", controller.findAll);
-app.put("/grade/:id", controller.update);
-app.delete("/grade/:id", controller.remove);
-app.delete("/grades/", controller.removeAll);
+app.post('/grade', controller.create);
+app.get('/grade/:id', controller.findOne);
+app.get('/grades', controller.findAll);
+app.put('/grade/:id', controller.update);
+app.delete('/grade/:id', controller.remove);
+app.delete('/grades/', controller.removeAll);
 
 app.listen(process.env.PORT || 8081, () => {
   console.log(`Servidor em execucao na porta ${process.env.PORT}`);
